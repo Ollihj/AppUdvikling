6f5ef98b4244d0d0a6560e22f5ac33ffc7686a21
Mathias-Maier <mathiasmr2002@gmail.com>
2026-04-08 10:42:09 +0200

    Login service

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -3,13 +3,20 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using WebApp;
 using WebApp.Service;
 
+using Blazored.LocalStorage;
+using Blazored.SessionStorage;
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddBlazoredSessionStorage();
 
 builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 // Skift ItemServiceMock til ItemServiceHttp for at tale med server
 builder.Services.AddSingleton<IItemService, ItemServiceMock>();
+builder.Services.AddSingleton<LoginService>();
 
 await builder.Build().RunAsync();
---
+++ WebApp/Service/LoginService.cs
@@ -0,0 +1,37 @@
+using Core.Models;
+using WebApp.Model;
+
+namespace WebApp.Service;
+
+public class LoginService
+{
+    private List<User> mUsers;
+    
+    public LoginService()
+    {
+        mUsers = [
+            new User { Name = "Anna", Password = "1234", Role = "admin" },
+            new User { Name = "Signe", Password = "2345", Role="admin"},
+            new User { Name="Ida", Password = "3456", Role="admin"},
+            new User { Name="Frida", Password = "12345", Role="admin"}
+        ];
+    }
+
+    /// <summary>
+    /// Validation of credentials
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="password"></param>
+    /// <returns>a user object if combination of username and password is known. Otherwise,
+    /// it will return null</returns>
+    public User? ValidLogin(string name, string password)
+    {
+        foreach (User u in mUsers)
+            if (u.Name == name && u.Password == password)
+            {
+                return u;
+            }
+        return null;
+    }
+    
+}
